95858d6e4bf8f3a47202cc344d18233ff058ce8c
Aliya <autyabaeva@mail.ru>
2022-11-26 19:09:05 +0400

    Task 2

---
+++ Homework_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input first number");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int second = Convert.ToInt32(Console.ReadLine());
+int max = first;
+if(second > max)
+{
+    Console.WriteLine("max = "+ second);
+}
+else
+{
+    Console.WriteLine("max = "+ first);
+}
