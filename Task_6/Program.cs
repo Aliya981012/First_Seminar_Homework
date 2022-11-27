bd96db6929896c25d772d32bb1c690871c958dcc
Aliya <autyabaeva@mail.ru>
2022-11-27 15:46:08 +0400

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Input number");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if(number % 2 == 0)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+        
+    }
+}
