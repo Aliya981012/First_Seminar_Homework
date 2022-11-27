4ae77e93b68f8da85f70ff31212f76991e12f9e2
Aliya <autyabaeva@mail.ru>
2022-11-27 14:29:48 +0400

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input first number");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number");
+int third = Convert.ToInt32(Console.ReadLine());
+int max = first;
+if(max < second)
+{
+    max = second;
+}
+if(max < third)
+{
+    max = third;
+}
+Console.WriteLine(max);
