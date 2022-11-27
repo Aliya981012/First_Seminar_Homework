ddcc1328ac0ebb6e699482053e6d886e9292c6cb
Aliya <autyabaeva@mail.ru>
2022-11-27 16:48:44 +0400

    Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input number that is greater than zero");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= number)
+{
+    if(count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
